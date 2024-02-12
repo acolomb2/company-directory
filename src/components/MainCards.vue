4fb6a5afd9ee8e60278a558289de13c3ff6c9881
Andres Colombon <acolomb2@stu.southtexascollege.edu>
2024-02-11 23:33:01 -0600

    Card data and style done

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,9 +1,9 @@
 <script setup>
-import MainCardSingle from './MainCardSingle.vue';
+    import MainCardSingle from './MainCardSingle.vue';
 </script>
 
 <template>
-
-<MainCardSingle v-for="card in 20" :key="card"></MainCardSingle>
-
+    <div class="grid grid-cols-5 gap-10 p-10">
+        <MainCardSingle v-for="card in 20" :key="card"></MainCardSingle>
+    </div>
 </template>
